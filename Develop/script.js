ce126239c970c38faf42c95efcf7b8043ff954ec
Hannah Bush <heb.rutgers@gmail.com>
2022-01-23 21:40:57 -0500

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+//password prompts:
+//password length - 8-128 chars
+//character types - lowercase, uppercase, numeric, special
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
