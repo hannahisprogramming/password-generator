5cf55395cb9849e1553c7fb7ed608c00afa33ef8
Hannah Bush <heb.rutgers@gmail.com>
2022-02-09 15:34:06 -0500

    character options added

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 //password prompts:
 //password length - 8-128 chars
-//character types - lowercase, uppercase, numeric, special
+//character types - lowercase, uppercase, numeric, special, all
 function generatePassword () {
   var passLength = parseInt(window.prompt("Enter password length: "));
   if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
     return "Please enter a valid number between 8 and 128";
   }
+  var lcase = window.confirm("Do you want lowercase characters?");
+  var ucase = window.confirm("Do you want uppercase characters?");
+  var numeric = window.confirm("Do you want numeric characters?");
+  var special = window.confirm("Do you want special characters?");
+  
+  var characters = [];
+  if (lcase){
+    characters+="abcdefghijklmnopqrstuvwxyz";
+  }
+  if (ucase){
+    characters+="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (numeric){
+    characters+="1234567890";
+  }
+  if (special){
+    characters+="!@#$%^&*()?/-_";
+  }
+
+  if (characters.length === 0) {
+    return "Please choose at least one option.";
+  }
+
+  
 }
 
 // Get references to the #generate element
