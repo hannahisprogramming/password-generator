2ec3eb462b8ad8976f427792fd7f7e27cbc6b6d5
Hannah Bush <heb.rutgers@gmail.com>
2022-02-09 15:21:34 -0500

    password length

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //password prompts:
 //password length - 8-128 chars
 //character types - lowercase, uppercase, numeric, special
-
-
+function generatePassword () {
+  var passLength = parseInt(window.prompt("Enter password length: "));
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    return "Please enter a valid number between 8 and 128";
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
